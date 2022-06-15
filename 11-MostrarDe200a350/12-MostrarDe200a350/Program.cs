420d2330d98674f57d4dad0aeba1cd19356675a0
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:26:52 -0300

    Add files via upload

---
+++ 11-MostrarDe200a350/12-MostrarDe200a350/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _12_MostrarDe200a350
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 200; i <= 350; i++)
+            {
+                Console.WriteLine("Los numeros son " + i);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
